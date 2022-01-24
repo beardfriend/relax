78f95ea17d682a7fa1cb861fe71aae00ed9644f0
beardfriend <beardfriend21@gmail.com>
2022-01-24 15:07:54 +0900

    @change 절대경로

--- apps/server-hire/src/app.ts
+++ apps/server-hire/src/app.ts
@@ -1,6 +1,6 @@
-import useDotenv from '@Initializations/express/lib/useDotenv';
-import initPostgres from '@Initializations/database/initPostgres';
-import initExpress from '@Initializations/express/initExpress';
+import useDotenv from '@SH/Initializations/express/lib/useDotenv';
+import initPostgres from '@SH/Initializations/database/initPostgres';
+import initExpress from '@SH/Initializations/express/initExpress';
 
 useDotenv();
 console.log('initializing postgres ....');
