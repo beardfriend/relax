daef70049a8fb535dbc4a945b0fc2c6d6fd50517
beardfriend <beardfriend21@gmail.com>
2022-02-02 00:29:05 +0900

    @add controller sample

--- apps/server-hire/src/controllers/user/login.ts
+++ apps/server-hire/src/controllers/user/login.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import { incorrectPassword, loginSuccess, noUser } from '@Constants/Messages';
 import { findUser } from '@SH/Services/user/user';
-import { getManager } from 'typeorm';
 
 async function login(req: Request, res: Response) {
-  const { email } = req.body;
-  const manager = await getManager();
-  const findMail = findUser(manager, email);
-  console.log(findMail);
-  res.send({ msg: 'hello' });
+  const { email, password } = req.body;
+  const user = await findUser(email);
+  if (user === undefined) {
+    return res.status(noUser.statusCode).json({ msg: noUser.message, category: noUser.category });
+  }
+  const isCorrectPassword = await bcrypt.compare(password, user.password);
+  if (!isCorrectPassword) {
+    return res
+      .status(incorrectPassword.statusCode)
+      .json({ msg: incorrectPassword.message, category: incorrectPassword.category });
+  }
+  return res.status(loginSuccess.statusCode).json({ msg: loginSuccess.message, category: loginSuccess.category });
 }
 
 export default login;
