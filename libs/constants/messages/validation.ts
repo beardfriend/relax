93c8b18516c7b2edf29c377129939c6b096ec041
beardfriend <beardfriend21@gmail.com>
2022-02-02 11:18:49 +0900

    @change validation url

--- libs/constants/messages/validation.ts
+++ libs/constants/messages/validation.ts
@@ -1,6 +1,6 @@
-import { validation } from '@Libs/interface/error';
+import { response } from '@Libs/interface/response';
 
-export const validationError: validation = {
+export const validationError: response = {
   statusCode: 400,
   category: 'validation',
   message: '데이터베이스 형식과 일치하지 않습니다.',
