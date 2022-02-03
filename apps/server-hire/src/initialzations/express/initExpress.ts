b5bcef97f1367db16b6d0f8c199f274a4c8e4372
beardfriend <beardfriend21@gmail.com>
2022-02-03 17:49:36 +0900

    @add CORS설정 임시

--- apps/server-hire/src/initialzations/express/initExpress.ts
+++ apps/server-hire/src/initialzations/express/initExpress.ts
@@ -2,10 +2,17 @@ import express from 'express';
 import useBodyParser from '@SH/Initializations/express/lib/useBodyParser';
 import setRoute from '@SH/Initializations/express/lib/setRoute';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const initExpress = () => {
   const app = express();
   app.use(cookieParser());
+  app.use((_req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  app.use(cors({ credentials: true, origin: true }));
   useBodyParser(app);
   setRoute(app);
   const port = process.env.EXPRESS_PORT;
