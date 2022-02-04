63b064e78cca24eaab1dbe0700f678fea391aa5f
beardfriend <beardfriend21@gmail.com>
2022-02-04 18:56:43 +0900

    @change logout logic

--- apps/server-hire/src/controllers/user/logout.ts
+++ apps/server-hire/src/controllers/user/logout.ts
@@ -1,3 +1,5 @@
+import token from '@Libs/constants/token';
+import { findCookieValue, splitCookie } from '@Libs/utils/cookie';
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
@@ -7,22 +9,20 @@ async function logout(req: Request, res: Response) {
   if (cookies === undefined || process.env.JWT === undefined) {
     return res.status(500).send({ msg: '서버 장애' });
   }
-  const cookie = cookies.split('; ');
-  const data = cookie.filter((datas) => !datas.indexOf('relaxLogin'));
 
-  if (data.length === 0) {
-    return res.status(400).send({ msg: '발급된 토큰 없음' });
+  const splitedCookies = splitCookie(cookies);
+  const loginToken = findCookieValue(splitedCookies, token.LOGIN);
+  if (loginToken === false) {
+    return res.status(500).send({ msg: '서버 장애' });
   }
 
-  const relaxLoginToken = data[0].split('=')[1];
-
   try {
-    await jwt.verify(relaxLoginToken, process.env.JWT);
-    res.cookie('relaxLogin', '', { maxAge: 0, httpOnly: true });
+    await jwt.verify(loginToken, process.env.JWT);
+    res.cookie(token.LOGIN, '', { maxAge: 0, httpOnly: true });
     return res.send({ msg: '로그아웃 됨' });
   } catch (error) {
     console.log(error);
-    return res.status(400).send({ msg: '토큰 키 불일치' });
+    return res.status(400).send({ msg: '서버에서 서명한 토큰이 아닙니다.' });
   }
 }
 
