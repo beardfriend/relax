f08e33bc4a6a9b7c65f222e5a85c87d4fdc86652
beardfriend <beardfriend21@gmail.com>
2022-03-12 15:12:57 +0900

    @add switchYogaType Reverse

--- libs/utils/switch.ts
+++ libs/utils/switch.ts
@@ -15,6 +15,21 @@ export function switchYogaType(key: string) {
   }
 }
 
+export function switchYogaTypeReverse(key: yogaSortType) {
+  switch (key) {
+    case yogaSortType.IYENGAR:
+      return 'IYENGAR';
+    case yogaSortType.HATA:
+      return 'HATA';
+    case yogaSortType.ASHTANGA:
+      return 'ASHTANGA';
+    case yogaSortType.FLYING:
+      return 'FLYING';
+    default:
+      throw new Error('존재하지 않는 타입입니다.');
+  }
+}
+
 export function swtichImageCategory(key: string) {
   switch (key) {
     case 'ACADEMY_LOGO':
@@ -38,6 +53,16 @@ export function swtichLoginType(uniqueKey: string | number, loginType: 'normal' 
     return { kakao_id: uniqueKey };
   }
   return { email: uniqueKey };
+}
+
+export function switchLoginType2(loginType: 'normal' | 'kakao' | 'google') {
+  if (loginType === 'google') {
+    return 'user.google_id =:uniqueKey';
+  }
+  if (loginType === 'kakao') {
+    return 'user.kakao_id =:uniqueKey';
+  }
+  return 'user.email =:uniqueKey';
 }
 
 export function swtichEnv(key: string) {
