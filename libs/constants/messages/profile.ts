00a694560cbe9aae4137a71b1c2c189a2f7db88a
beardfriend <beardfriend21@gmail.com>
2022-03-12 15:13:08 +0900

    @add academyProfile Message

--- libs/constants/messages/profile.ts
+++ libs/constants/messages/profile.ts
@@ -5,3 +5,15 @@ export const profileGetSuccess: response = {
   category: 'profile',
   message: '선생님 프로필 요청에 성공하였습니다.',
 };
+
+export const profileCreate: response = {
+  statusCode: 200,
+  category: 'profile',
+  message: '선생님 프로필 생성요청에 성공하였습니다.',
+};
+
+export const profileUpdate: response = {
+  statusCode: 200,
+  category: 'profile',
+  message: '선생님 프로필이 수정요청에 성공하였습니다.',
+};
