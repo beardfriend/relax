32625a809ec3363f785f7a279166e2afbbad18cb
beardfriend <beardfriend21@gmail.com>
2022-04-05 16:37:27 +0900

    @add 요가 태그 불러오기

--- apps/server-hire/src/controllers/tags/yoga.ts
+++ apps/server-hire/src/controllers/tags/yoga.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from 'express';
+import { getYogaTagsSuccess } from '@Libs/constants/messages/tags';
 import { getManager } from 'typeorm';
 
 export default async function yoga(req: Request, res: Response) {
   const manager = getManager();
   const data = await manager.query(`SELECT * FROM yoga_tags WHERE name LIKE '%${req.query.search}%'`);
-  return res.send(data);
+  return res.status(getYogaTagsSuccess.statusCode).send({
+    msg: getYogaTagsSuccess.message,
+    category: getYogaTagsSuccess.category,
+    data,
+  });
 }
---
+++ libs/constants/messages/tags.ts
@@ -0,0 +1,7 @@
+import { response } from '@Libs/interface/response';
+
+export const getYogaTagsSuccess: response = {
+  statusCode: 200,
+  category: 'tags',
+  message: '요가 태그 요청에 성공하였습니다.',
+};
